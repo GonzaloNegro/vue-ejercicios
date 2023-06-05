3199afd31a922716c41bfc4cacdeb798d2bb6124
Gonzalo Negro <gonzalonnegro@gmail.com>
2023-06-05 19:48:30 -0300

    style

--- Componentes/app.js
+++ Componentes/app.js
@@ -5,9 +5,30 @@ const Header = {
     titulo: String
   },
   template: `
-    <h1>{{titulo}}</h1>
+    <div class="titulo">
+      <img class="" src="./burger.png">
+      <h1>{{titulo}}</h1>
+      <img class="" src="./espatula.png">
+    </div>
+      `
+} 
+
+const Footer = {
+  props: {
+    subtitulo: String
+  },
+  template: `
+  <div class="foot">
+    <h2>{{subtitulo}}</h2>
+  </div>
   `
 } 
+
+const cart = {
+  template: `
+    <button class="btnFltante"><i class="fa-solid fa-cart-shopping"></i></button>
+  `
+}
 
 const Contador = {
   template:`
@@ -28,15 +49,17 @@ const Card = {
     portada: String,
     peso: Number,
     calorias: Number,
-    costo: Number
+    costo: Number,
+    ingredientes: String
   },
   template: `
   <div class="card">
     <div class="img-bur">
       <img class="" :src="portada" :title="titulo">
+      <div class="texto-encima">{{ingredientes}}</div>
     </div>
       <br><br>
-      <p class="infor">{{ titulo }}</p>
+      <p class="tittle">{{ titulo }}</p>
       <table>
         <tr>
           <td>Peso</td>
@@ -47,7 +70,7 @@ const Card = {
           <td>{{calorias}}Kcal</td>
         </tr>
       </table>
-      <p class="infor">$ {{ costo }}</p>
+      <p class="infor">Precio: $ {{ costo }}</p>
       <card-count/>
   </div>
   `
@@ -68,6 +91,7 @@ const MainCards = {
     :peso="burger.peso"
     :calorias="burger.calorias"
     :costo="burger.costo"
+    :ingredientes="burger.ingredientes"
     class="cont-cards"
     ></card-component>
   </div>
@@ -78,7 +102,9 @@ const MainCards = {
 createApp({
   components: {
     'header-component': Header,
-    'main-cards': MainCards
+    'main-cards': MainCards,
+    'footer-component': Footer,
+    'cart-component': cart
   },
   data() {
     return {
@@ -89,7 +115,8 @@ createApp({
           costo: 2100.00,
           peso: 700,
           calorias: 200,
-          portada: "https://punto33.com/wp-content/uploads/2022/12/Bomba-33.jpg"
+          portada: "https://punto33.com/wp-content/uploads/2022/12/Bomba-33.jpg",
+          ingredientes: "INGREDIENTES QUE VA A LLEVAR LA HAMBURGUESA"
         },
         {
           id: 2,
@@ -97,7 +124,8 @@ createApp({
           costo: 2100.00,
           peso: 700,
           calorias: 280,
-          portada: "https://punto33.com/wp-content/uploads/2022/12/Especial-33.jpg"
+          portada: "https://punto33.com/wp-content/uploads/2022/12/Especial-33.jpg",
+          ingredientes: "INGREDIENTES QUE VA A LLEVAR LA HAMBURGUESA"
         },
         {
           id: 3,
@@ -105,7 +133,8 @@ createApp({
           costo: 2000.00,
           peso: 800,
           calorias: 600,
-          portada: "https://punto33.com/wp-content/uploads/2022/12/Big-33.jpg"
+          portada: "https://punto33.com/wp-content/uploads/2022/12/Big-33.jpg",
+          ingredientes: "INGREDIENTES QUE VA A LLEVAR LA HAMBURGUESA"
         },
         {
           id: 4,
@@ -113,7 +142,8 @@ createApp({
           costo: 1800.00,
           peso: 700,
           calorias: 500,
-          portada: "https://punto33.com/wp-content/uploads/2022/12/Barbacoa-33.jpg"
+          portada: "https://punto33.com/wp-content/uploads/2022/12/Barbacoa-33.jpg",
+          ingredientes: "INGREDIENTES QUE VA A LLEVAR LA HAMBURGUESA"
         },
         {
           id: 5,
@@ -121,7 +151,8 @@ createApp({
           costo: 1750.00,
           peso: 500,
           calorias: 350,
-          portada: "https://punto33.com/wp-content/uploads/2022/12/Doble-cheese.jpg"
+          portada: "https://punto33.com/wp-content/uploads/2022/12/Doble-cheese.jpg",
+          ingredientes: "INGREDIENTES QUE VA A LLEVAR LA HAMBURGUESA"
         },
         {
           id: 6,
@@ -129,7 +160,8 @@ createApp({
           costo: 1850.00,
           peso: 600,
           calorias: 6500,
-          portada: "https://punto33.com/wp-content/uploads/2022/12/cuartodelibra-1024x1024.png"
+          portada: "https://punto33.com/wp-content/uploads/2022/12/cuartodelibra-1024x1024.png",
+          ingredientes: "INGREDIENTES QUE VA A LLEVAR LA HAMBURGUESA"
         },
         {
           id: 7,
@@ -137,7 +169,8 @@ createApp({
           costo: 1900.00,
           peso: 700,
           calorias: 1000,
-          portada: "https://punto33.com/wp-content/uploads/2022/12/bluecheese33-1024x1024.png"
+          portada: "https://punto33.com/wp-content/uploads/2022/12/bluecheese33-1024x1024.png",
+          ingredientes: "INGREDIENTES QUE VA A LLEVAR LA HAMBURGUESA"
         },
         {
           id: 8,
@@ -145,7 +178,8 @@ createApp({
           costo: 2000.00,
           peso: 600,
           calorias: 400,
-          portada: "https://punto33.com/wp-content/uploads/2022/12/Mildis-Ahumada.jpg"
+          portada: "https://punto33.com/wp-content/uploads/2022/12/Mildis-Ahumada.jpg",
+          ingredientes: "INGREDIENTES QUE VA A LLEVAR LA HAMBURGUESA"
         }
       ]
 
